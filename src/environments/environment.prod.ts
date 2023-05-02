be177cdf93d8046215cd007c72698327689eb889
mahesh <mthakur@sevadev.com>
2023-05-02 15:06:23 +0545

    configured firebase deplyment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:4000'
+    apiUrl: 'http://localhost:4000',
+    OPENAI_API_KEY: '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'
 };
